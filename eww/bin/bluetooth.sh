65bfd63aaad6c2c546e4b410bc96dc5dafe83bc2
Daylin Morgan <daylinmorgan@gmail.com>
2023-07-17 11:48:08 -0500

    update bluetooth command to prevent false positive

--- eww/bin/bluetooth.sh
+++ eww/bin/bluetooth.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]; then
+if [ "$(bluetoothctl show | grep "Powered: yes" | wc -c)" -eq 0 ]; then
 	echo ""
 else
-	if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]; then
+	if [ "$(bluetoothctl devices Connected | wc -c)" -eq 0 ]; then
 		echo ""
 	else
 		echo ""
